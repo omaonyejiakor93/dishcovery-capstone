8037ccf60ce7dbae2d3ebf3ab053f08c249ec3a1
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-09-06 12:50:45 +0100

    Add Navbar with Tailwind styling and temporary preview text

--- dishcovery/src/App.jsx
+++ dishcovery/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import RecipeDetail from './pages/RecipeDetail'
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import RecipeDetail from "./pages/RecipeDetail"
+import Favorites from "./pages/Favorites"
+import Search from "./pages/Search"
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipe/:id" element={<RecipeDetail />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      {/* Temporary Preview Section */}
+      <div className="text-center py-10 text-gray-700">
+        <h1 className="text-3xl font-bold">Welcome to Dishcovery 🍲</h1>
+        <p className="mt-3 text-lg">Your Navbar is working! 🚀</p>
+      </div>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </div>
   )
 }
+
+export default App
